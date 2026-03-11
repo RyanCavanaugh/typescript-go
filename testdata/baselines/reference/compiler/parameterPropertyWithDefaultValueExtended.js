0530c9f2e1b3dbcc2a85afdd3a2ac31b772bf8f8
Wesley Wigham <wwigham@gmail.com>
2026-03-11 15:27:46 -0700

    Accept baselines

--- testdata/baselines/reference/compiler/parameterPropertyWithDefaultValueExtended.js
+++ testdata/baselines/reference/compiler/parameterPropertyWithDefaultValueExtended.js
@@ -65,8 +65,8 @@ export declare class WithDefault {
     constructor(timestamp?: Date);
 }
 export declare class WithoutDefault {
-    readonly timestamp?: Date | undefined;
-    constructor(timestamp?: Date | undefined);
+    readonly timestamp?: Date;
+    constructor(timestamp?: Date);
 }
 export declare class ExplicitUndefined {
     readonly timestamp: Date | undefined;
